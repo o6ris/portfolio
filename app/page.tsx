9b713b6a39cc0b7ca72e5d2a4d5c082ed0d2f362
o6ris <loic.tsiry@gmail.com>
2025-03-06 14:57:45 -0700

    style(page home): style the chat and it's section

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import useChat from "@/modules/clients/hooks/useChat";
 import InputField from "@core/ui/Fields/InputField/InputField";
 import BasicButton from "@/core/ui/Button/BasicButton";
+import Icon from "@/core/ui/Icons/Icon";
 
 export default function Home() {
   const { askChat, messages, setQuestion, question } = useChat();
@@ -14,19 +15,25 @@ export default function Home() {
           <h1>Love turing Ideas into life</h1>
         </section>
         {/* CHAT */}
-        <section className="flex flex-col items-center justify-center bg-purple-500 w-full">
+        <section className="flex flex-col items-center justify-center bg-radial from-slate-800 from-20% to-slate-950 w-full">
           <article className="flex flex-col items-center justify-center w-full h-full">
-            <div className="flex flex-col gap-2 w-2/3 h-2/3 p-2 bg-green-100">
+            <div className="flex flex-col gap-4 w-3/4 h-3/4 p-4 bg-gradient-to-r from-slate-950 to-slate-900 shadow-purple-3xl rounded-xl">
               {/* ANSWERS */}
-              <div className="h-full bg-green-500 p-4">
-                {messages.map((message, i: number) => {
-                  return (
-                    <div key={i}>
-                      <div>{message.question}</div>
-                      <div>{message.answer}</div>
+              <div className="h-full p-4 overflow-auto no-scrollbar">
+                {messages.map((message, i) => (
+                  <div className="flex flex-col gap-4 mb-8" key={i}>
+                    <div className="flex justify-start mb-1">
+                      <div className="bg-slate-800 shadow-md p-2 rounded-lg max-w-3/4 text-fuchsia-200">
+                        {message.question}
+                      </div>
                     </div>
-                  );
-                })}
+                    <div className="flex justify-end">
+                      <div className="bg-gradient-to-r from-fuchsia-900 to-purple-600 shadow-md p-2 rounded-lg max-w-3/4 text-fuchsia-200">
+                        {message.answer}
+                      </div>
+                    </div>
+                  </div>
+                ))}
               </div>
               {/* QUESTIONS */}
               <div className="flex flex-row gap-2">
@@ -34,12 +41,15 @@ export default function Home() {
                   value={question}
                   onValueChange={(value) => setQuestion(value)}
                   variant="bordered"
-                  classNames={{ inputWrapper: " flex-1 bg-blue-500" }}
+                  classNames={{
+                    inputWrapper: "flex-1 border-2 border-fuchsia-900 rounded-2xl ",
+                    input: "p-4 text-slate-500"
+                  }}
                 />
                 <BasicButton
                   onPress={() => askChat()}
-                  content=">"
-                  className="w-auto flex-none bg-yellow-800"
+                  startContent={<Icon name="SendHorizontal" strokeWidth={1} size={26} color="white" />}
+                  className="w-auto flex-none bg-gradient-to-r from-fuchsia-900 to-purple-600 rounded-3xl shadow-md shadow-purple-500/50"
                 />
               </div>
             </div>
