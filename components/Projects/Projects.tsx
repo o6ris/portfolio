40c58e5064adb0f95293387e62553e2cdfb964fd
o6ris <loic.tsiry@gmail.com>
2025-03-12 11:38:25 -0600

    feat(myProjects & projects): create JSON to store projects infos and display it with a map in Projects components

--- components/Projects/Projects.tsx
+++ components/Projects/Projects.tsx
@@ -1,94 +1,53 @@
 "use client";
 
 import { motion } from "framer-motion";
+import Image from "next/image";
+import myProjects from "@/modules/clients/utils/myProjects";
 
 function Projects() {
   return (
     <section className="grid grid-flow-col grid-rows-2 gap-6">
-      <motion.article
-        initial="hidden"
-        whileInView="visible"
-        variants={{
-          hidden: { opacity: 0, scale: 0.8 },
-          visible: {
-            opacity: 1,
-            scale: 1,
-            transition: { duration: 0.5, delay: 0.2 },
-          },
-        }}
-        viewport={{ once: true, amount: 0.8 }}
-        className="bg-red-800 h-56 p-6 w-lg rounded-lg shadow-lg"
-      >
-        <h2 className="text-xl font-semibold text-white">GrindPal</h2>
-        <p className="text-white text-sm">
-          A workout tracker built for efficiency, replacing spreadsheets with a
-          sleek, visual interface. Track progress, log workouts, and analyze
-          performance with interactive charts.
-        </p>
-      </motion.article>
-      <motion.article
-        initial="hidden"
-        whileInView="visible"
-        variants={{
-          hidden: { opacity: 0, scale: 0.8 },
-          visible: {
-            opacity: 1,
-            scale: 1,
-            transition: { duration: 0.5, delay: 0.4 },
-          },
-        }}
-        viewport={{ once: true, amount: 0.8 }}
-        className="bg-red-800 h-56 p-6 w-lg rounded-lg shadow-lg"
-      >
-        <h2 className="text-xl font-semibold text-white">My Portfolio</h2>
-        <p className="text-white text-sm">
-          An interactive showcase of my work, featuring a chatbot version of me
-          that streams real-time responses and and call booking. Includes smooth
-          animations, projects showcase and more.
-        </p>
-      </motion.article>
-      <motion.article
-        initial="hidden"
-        whileInView="visible"
-        variants={{
-          hidden: { opacity: 0, scale: 0.8 },
-          visible: {
-            opacity: 1,
-            scale: 1,
-            transition: { duration: 0.5, delay: 0.6 },
-          },
-        }}
-        viewport={{ once: true, amount: 0.8 }}
-        className="bg-red-800 h-56 p-6 w-lg rounded-lg shadow-lg"
-      >
-        <h2 className="text-xl font-semibold text-white">Luo</h2>
-        <p className="text-white text-sm">
-          A workout tracker built for efficiency, replacing spreadsheets with a
-          sleek, visual interface. Track progress, log workouts, and analyze
-          performance with interactive charts.
-        </p>
-      </motion.article>
-      <motion.article
-        initial="hidden"
-        whileInView="visible"
-        variants={{
-          hidden: { opacity: 0, scale: 0.8 },
-          visible: {
-            opacity: 1,
-            scale: 1,
-            transition: { duration: 0.5, delay: 0.8 },
-          },
-        }}
-        viewport={{ once: true, amount: 0.8 }}
-        className="bg-red-800 h-56 p-6 w-lg rounded-lg shadow-lg"
-      >
-        <h2 className="text-xl font-semibold text-white">LSA</h2>
-        <p className="text-white text-sm">
-          An interactive showcase of my work, featuring a chatbot version of me
-          that streams real-time responses and and call booking. Includes smooth
-          animations, projects showcase and more.
-        </p>
-      </motion.article>
+      {myProjects.map((project, i) => {
+        return (
+          <motion.article
+            key={i}
+            initial="hidden"
+            whileInView="visible"
+            variants={{
+              hidden: { opacity: 0, scale: 0.8 },
+              visible: {
+                opacity: 1,
+                scale: 1,
+                transition: { duration: 0.5, delay: 0.2 },
+              },
+            }}
+            viewport={{ once: true, amount: 0.8 }}
+            className="bg-red-800 h-56 p-2 w-lg rounded-lg shadow-lg bg-gradiant-primary shadow-purple-3xl"
+          >
+            <div className="relative p-4 h-full rounded-sm overflow-hidden bg-slate-950">
+              <Image
+                src={project.image}
+                alt={project.name}
+                layout="fill"
+                objectFit="cover"
+                className="absolute inset-0 z-0 blur-xs opacity-50"
+              />
+              <div className="relative z-10 p-4">
+                <h2 className="text-xl font-semibold text-white">
+                  {project.name} |{" "}
+                  <span className="text-base font-regular text-slate-600">
+                    {project.position}
+                  </span>
+                </h2>
+                <p className="text-white text-sm">{project.description}</p>
+                {project.stack.map((el, j) => {
+                  return <div key={j}>{el}</div>;
+                })}
+              </div>
+            </div>
+          </motion.article>
+        );
+      })}
     </section>
   );
 }
---
+++ modules/clients/utils/myProjects.ts
@@ -0,0 +1,51 @@
+const myProjects = [
+  {
+    name: "Grindpal",
+    position: "Full stack Web developer",
+    description:
+      "Web application designed for seamless workout tracking. Log workouts effortlessly, monitor progress over time, and analyze performance through interactive charts, all in one platform.",
+    image: "/grindpal.jpg",
+    stack: [
+      "Javascript",
+      "Next.js",
+      "Node.js",
+      "MongoDB",
+      "NextAuth",
+      "Heroui",
+    ],
+  },
+  {
+    name: "Portfolio",
+    position: "Front-end Web developer",
+    description:
+      "An interactive showcase of my work, featuring a chatbot version of me that streams real-time responses and call booking. Includes smooth animations, projects showcase and more.",
+    image: "/portfolio.jpg",
+    stack: ["Typescript", "Next.js", "React.js"],
+  },
+  {
+    name: "LUO",
+    position: "Full-Stack Web Developer",
+    description:
+      "A rental management web application that helps users track rentals, manage notifications, and visualize rental data with interactive charts.",
+    image: "/luo.jpg",
+    stack: [
+      "Typescript",
+      "Next.js",
+      "React.js",
+      "MongoDB",
+      "Heroui",
+      "FullCalendar",
+      "Chart.js",
+    ],
+  },
+  {
+    name: "LSA",
+    position: "Front-End Web Developer",
+    description:
+      "An insurance aggregator web app that streamlines contract creation, integrates a shopping cart system, and automates proposal generation via PDF export.",
+    image: "/lsa.jpg",
+    stack: ["React.js", "Python", "Google Drive API"],
+  },
+];
+
+export default myProjects;
