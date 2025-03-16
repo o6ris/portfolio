b8d0d05e11077a4c7d4e1c31d9ae404f366ef608
o6ris <loic.tsiry@gmail.com>
2025-03-16 11:26:53 -0600

    fixed(page home): fixed bad behavior on chat when typing lot of messages

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,11 @@ import Contact from "@/components/Contact/Contact";
 export default function Home() {
   return (
     <main className="min-h-screen">
-      <section className="flex flex-col lg:grid lg:grid-cols-[0.8fr_1.2fr] lg:h-dvh">
-        <section className="flex items-center justify-center w-full lg:h-full">
+      <section className="flex flex-col lg:flex-row lg:h-dvh">
+        <section className="flex flex-[0.8] items-center justify-center w-full lg:h-full">
           <Hero />
         </section>
-        <section className="hidden lg:flex flex-col items-center justify-center w-full">
+        <section className="hidden lg:flex flex-[1.2] flex-col items-center justify-center w-full">
           <Chat />
         </section>
       </section>
